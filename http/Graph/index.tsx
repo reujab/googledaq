e0614c1944e078c4a856781fc2e4c20eea280aab
reujab <reujab@gmail.com>
2019-03-18 18:45:23 -0400

    tweaked graph

--- http/Graph/index.tsx
+++ http/Graph/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
-import { Line } from "react-chartjs-2"
 import { GraphedStock } from ".."
+import { Line } from "react-chartjs-2"
+import { displayMoney } from "../common"
 
 interface Props {
 	stock: null | GraphedStock
@@ -21,6 +22,34 @@ export default class Graph extends React.Component<Props> {
 				// The animation seems to be a bit glitched.
 				animation: {
 					duration: 0,
+				},
+
+				scales: {
+					yAxes: [{
+						ticks: {
+							callback(cents) {
+								return displayMoney(cents)
+							},
+						},
+					}],
+				},
+
+				legend: {
+					display: false,
+				},
+
+				title: {
+					display: true,
+					fontSize: 24,
+					text: this.props.stock.name,
+				},
+
+				tooltips: {
+					displayColors: false,
+
+					callbacks: {
+						label: (tooltip) => displayMoney(this.props.stock.costs[tooltip.index])
+					},
 				},
 			}} />
 		)
