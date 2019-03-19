db8416be3a0396c8fe51627dbcbaee57f0dc693a
reujab <reujab@gmail.com>
2019-03-18 23:42:40 -0400

    tweaked stock view

--- http/Stock/index.tsx
+++ http/Stock/index.tsx
@@ -24,12 +24,11 @@ export default class Stock extends React.Component<Props, State> {
 				onClick={this.props.onClick}
 			>
 				<div className="stock-name">
-					{this.props.stock.name}
-					<br />
-					{this.props.stock.shares} Share{this.props.stock.shares === 1 ? "" : "s"}
+					<div>{this.props.stock.name}</div>
+					<div>{this.props.stock.shares} Share{this.props.stock.shares === 1 ? "" : "s"}</div>
 				</div>
 				<div className="stock-stats">
-					{displayMoney(this.props.stock.currentPrice)}
+					{displayMoney(this.props.stock.currentPrice)}/share
 					<br />
 					<div className={`stock-percentage ${this.getPercent() > 0 ? "green" : this.getPercent() < 0 ? "red" : "neutral"}`}>
 						<Icon icon={this.getPercent() >= 1 ? "double-chevron-up" : this.getPercent() <= -1 ? "double-chevron-down" : this.getPercent() > 0 ? "chevron-up" : this.getPercent() < 0 ? "chevron-down" : "small-minus"} />
